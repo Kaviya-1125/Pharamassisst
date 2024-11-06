4ecf08850167bfd479e1fdba39e99a6c392c1f75
Kaviya-1125 <kavoff1125@gmail.com>
2024-11-06 15:12:24 +0530

    add-pharmacy feature added

---
+++ src/main/java/com/example/pharmassisst/exceptionhandler/PharmacyExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.pharmassisst.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.pharmassisst.exception.NoPharmacyFoundException;
+import com.example.pharmassisst.exception.PharmacyNotFoundByIdException;
+import com.example.pharmassisst.utility.AppResponseBuilder;
+import com.example.pharmassisst.utility.ErrorStructure;
+
+public class PharmacyExceptionHandler {
+
+	private final AppResponseBuilder responseBuilder;
+
+	public PharmacyExceptionHandler(AppResponseBuilder responseBuilder) {
+		super();
+		this.responseBuilder = responseBuilder;
+	}
+
+	@ExceptionHandler(NoPharmacyFoundException.class)
+	public ResponseEntity<ErrorStructure> handleNoPharmacyFound(NoPharmacyFoundException ex) {
+
+		return responseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage(),"No pharmacies found based on the criteria");
+	}
+
+	@ExceptionHandler(PharmacyNotFoundByIdException.class)
+	public ResponseEntity<ErrorStructure> handlePharmacyFoundNotById(PharmacyNotFoundByIdException ex) {
+
+		return responseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage(),"No pharmacies found based on the id");
+	}
+
+}
---
+++ src/main/java/com/example/pharmassisst/repository/PharmacyRepository.java
@@ -0,0 +1,9 @@
+package com.example.pharmassisst.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.pharmassisst.entity.Pharmacy;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, String>{
+
+}
